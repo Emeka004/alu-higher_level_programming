3cabae91ab56a345e32f1eaa547a7921d46e6c3a
Your Name <you@example.com>
2025-03-14 11:10:08 -0700

    4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+"""
+Module: 4-from_json_string.py
+Function to return an object (Python data structure) represented by a JSON string.
+"""
+
+
+def from_json_string(my_str):
+    """Returns the object (Python data structure) represented by a JSON string"""
+    return json.loads(my_str)
+
+json_str = '{"name": "Alice", "age": 25}'
+my_obj = from_json_string(json_str)
+print(my_obj)
+
