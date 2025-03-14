b918d2768b6db485eee0b31585f8b76bd3141ac1
Your Name <you@example.com>
2025-03-14 10:01:44 -0700

    3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import json
 """
-Module: to_json_string.py
-Function to return the JSON representation of an object.
+Module: 3-to_json_string.py
+Function to return the JSON representation of an object as a string.
 """
 
 
