02a1af47b1e9fce73a793b40bde76b468eb8e3b0
Your Name <you@example.com>
2025-03-14 09:41:40 -0700

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,7 @@ Module: 0-read_file.py
 Function to read a file and print its content to stdout.
 """
 
+
 def read_file(filename=""):
     """Reads a UTF-8 file and prints its content to stdout"""
     with open(filename, 'r', encoding='utf-8') as file:
