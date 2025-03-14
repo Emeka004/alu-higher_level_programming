5c2d213f1d8ab4cb88644859c5f6c3df1e4a0f3c
Your Name <you@example.com>
2025-03-14 09:49:25 -0700

    1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module: 1-write_file.py
+Function to write a string to a file and return the number of characters written.
+"""
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file and returns the number of characters written"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
+
